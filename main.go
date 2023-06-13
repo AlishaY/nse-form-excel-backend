197cabba8b47eb6db53a27a0f6dd0a5f8aed21fe
AlishaY <36836010+AlishaY@users.noreply.github.com>
2023-06-13 14:34:55 +0800

    modified JobNo send as variable

--- main.go
+++ main.go
@@ -11,8 +11,6 @@ import (
 )
 
 type Job struct {
-	// ID       	int  `json:"id"`
-	// Item       	string  `json:"item"`
 	JobDate       		string  `json:"jobDate"`
 	ReferenceNo 		string 	`json:"referenceNo"`
 	CustomerName		string	`json:"customerName"`
@@ -23,18 +21,8 @@ type Job struct {
 var db *sql.DB
 
 func main() {
-	// Database connection configuration
-	// server := "WIN-OMPBT533FJ7"
-	// port := 1433
-	// user := "coadmin"
-	// password := "tms@1234"
-	// database := "CoTMS"
 
 	var err error
-	// db, err = sql.Open(
-	// 	"sqlserver",
-	// 	"sqlserver://coadmin:tms@1234@103.230.124.241:1433?database=CoTMS&connection+timeout=30",
-	// )
 	db, err = sql.Open(
 		"sqlserver",
 		"sqlserver://coadmin:alisha@1234@localhost?database=TODO&connection+timeout=30",
@@ -48,7 +36,7 @@ func main() {
 	router := gin.Default()
 
 	// Define your API endpoints
-	router.GET("/joborder", getJob)
+	router.GET("/joborder/:jobNo", getJob)
 
 	// Start the server
 	router.Run(":3000")
@@ -56,7 +44,8 @@ func main() {
 
 func getJob(c *gin.Context) {
 	fmt.Println("first line in getJob")
-	rows, err := db.Query("SELECT JobDate, ReferenceNo, CustomerName, DeliveryLocName, TruckNo FROM JobOrder WHERE JobNo='TDN0098789'")
+	jobNo := c.Param("jobNo")
+	rows, err := db.Query("SELECT JobDate, ReferenceNo, CustomerName, DeliveryLocName, TruckNo FROM JobOrder WHERE JobNo=@JobNo", sql.Named("JobNo", jobNo))
 	if err != nil {
 		fmt.Println("this is error")
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to get Jobs"})
@@ -67,16 +56,12 @@ func getJob(c *gin.Context) {
 
 	jobs := []Job{}
 	for rows.Next() {
-		fmt.Println("scan")
 		var job Job
 		err := rows.Scan(&job.JobDate, &job.ReferenceNo, &job.CustomerName, &job.DeliveryLocName, &job.TruckNo)
-		fmt.Println("scan 2")
 		if err != nil {
-			fmt.Println("scan 3")
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to scan row"})
 			return
 		}
-		fmt.Println("scan 4")
 		jobs = append(jobs, job)
 	}
 	fmt.Println("end")
