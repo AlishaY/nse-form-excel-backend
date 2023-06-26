94e161ad049e4b89521424cccefd40fb0e4efacc
AlishaY <36836010+AlishaY@users.noreply.github.com>
2023-06-26 14:58:21 +0800

    write using service account credentials

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module nse-form-excel-backend
 go 1.20
 
 require (
+	cloud.google.com/go/compute v1.19.3 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
 	github.com/bytedance/sonic v1.9.1 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/denisenkom/go-mssqldb v0.12.3 // indirect
@@ -16,6 +18,12 @@ require (
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/golang-sql/civil v0.0.0-20220223132316-b832511892a9 // indirect
 	github.com/golang-sql/sqlexp v0.1.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/s2a-go v0.1.4 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.2.4 // indirect
+	github.com/googleapis/gax-go/v2 v2.10.0 // indirect
 	github.com/jinzhu/gorm v1.9.16 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
@@ -29,11 +37,17 @@ require (
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
+	go.opencensus.io v0.24.0 // indirect
 	golang.org/x/arch v0.3.0 // indirect
-	golang.org/x/crypto v0.9.0 // indirect
-	golang.org/x/net v0.10.0 // indirect
-	golang.org/x/sys v0.8.0 // indirect
-	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/crypto v0.10.0 // indirect
+	golang.org/x/net v0.11.0 // indirect
+	golang.org/x/oauth2 v0.8.0 // indirect
+	golang.org/x/sys v0.9.0 // indirect
+	golang.org/x/text v0.10.0 // indirect
+	google.golang.org/api v0.128.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230530153820-e85fd2cbaebc // indirect
+	google.golang.org/grpc v1.55.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	gorm.io/driver/sqlserver v1.5.1 // indirect
--- main.go
+++ main.go
@@ -1,45 +1,188 @@
 package main
 
 import (
+	"io/ioutil"
+	"context"
 	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
-
+	// "errors"
+	// "os"
+	"strings"
+	// "path/filepath"
 	_ "github.com/denisenkom/go-mssqldb" // SQL Server driver
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"google.golang.org/api/option"
+	"google.golang.org/api/sheets/v4"
+	// "golang.org/x/net/context"
+	// "google.golang.org/api/googleapi"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 )
-
+//json uri redirect: http://localhost:8080/callback
 type Job struct {
-	JobDate       		string  `json:"jobDate"`
-	ReferenceNo 		string 	`json:"referenceNo"`
-	CustomerName		string	`json:"customerName"`
-	DeliveryLocName		string	`json:"deliveryLocName"`
-	TruckNo				string	`json:"truckNo"`
+	JobDate         string `json:"jobDate"`
+	ReferenceNo     string `json:"referenceNo"`
+	CustomerName    string `json:"customerName"`
+	DeliveryLocName string `json:"deliveryLocName"`
+	TruckNo         string `json:"truckNo"`
 }
 
 var db *sql.DB
 
 func main() {
-
-	var err error
-	db, err = sql.Open(
-		"sqlserver",
-		"sqlserver://coadmin:alisha@1234@localhost?database=TODO&connection+timeout=30",
-	)
-	if err != nil {
-		log.Fatalf("Failed to connect to the database: %v", err)
-	}
-	defer db.Close()
-
-	// Create a new Gin router
 	router := gin.Default()
 
-	// Define your API endpoints
+	// Enable CORS
+	router.Use(cors.Default())
+
+	// Define the API endpoints
 	router.GET("/joborder/:jobNo", getJob)
+	router.POST("/joborder/storeIssues", writeIssue)
+	router.POST("/joborder/write", writeDataHandler)
 
 	// Start the server
 	router.Run(":3000")
+}
+
+func writeDataHandler(c *gin.Context) {
+	// Path to your service account JSON key file
+	keyFile := "service_account.json"
+
+	// Create a new Google Sheets service client with the service account credentials
+	ctx := context.Background()
+	srv, err := sheets.NewService(ctx, option.WithCredentialsFile(keyFile))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to create Sheets service client",
+		})
+		return
+	}
+
+	// Specify the spreadsheet ID and range
+	spreadsheetID := "1IKRI_CasrSyOCPbyhsDKQmE0tJGTlNbP0tVRF9FHPEQ"
+	rangeValue := "sample1!A1:B3"
+
+	// Create the value range to write
+	values := [][]interface{}{
+		{"Value A1", "Value B1"},
+		{"Value A2", "Value B2"},
+		{"Value A2", "Value B2"},
+	}
+	valueRange := &sheets.ValueRange{
+		Values: values,
+	}
+
+	// Write the values to the spreadsheet
+	_, err = srv.Spreadsheets.Values.Update(spreadsheetID, rangeValue, valueRange).ValueInputOption("USER_ENTERED").Do()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to write data to spreadsheet" + err.Error(),
+		})
+		return
+	}
+
+	// Data successfully written
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Data written to spreadsheet",
+	})
+}
+
+func writeIssue(c *gin.Context) {
+	ctx := context.Background()
+
+	// service, err := getServiceClient(ctx)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	cl, err := getClient(ctx)
+        if err != nil {
+                log.Fatal(err)
+        }
+
+        sheetsService, err := sheets.New(cl)
+        if err != nil {
+                log.Fatal(err)
+        }
+	// The ID of the spreadsheet to update.
+	spreadsheetID := "1IKRI_CasrSyOCPbyhsDKQmE0tJGTlNbP0tVRF9FHPEQ" // Replace with your spreadsheet ID
+
+	// The A1 notation of the range to update.
+	rangeValue := "Sheet1!A1:D2" // Replace with the desired range
+
+	// The input data.
+	rb := &sheets.ValueRange{
+		Values: [][]interface{}{
+			{"Value A1", "Value B1", "Value C1", "Value D1"},
+			{"Value A2", "Value B2", "Value C2", "Value D2"},
+		},
+	}
+
+	// Append the values to the spreadsheet
+	resp, err := sheetsService.Spreadsheets.Values.Append(spreadsheetID, rangeValue, rb).ValueInputOption("USER_ENTERED").InsertDataOption("INSERT_ROWS").Context(ctx).Do()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Updated %d cells.\n", resp.Updates.UpdatedCells)
+}
+
+func getServiceClient(ctx context.Context) (*sheets.Service, error) {
+	credentialsFile := "credentials.json" // Update the filename to match your credentials file
+
+	// Read the credentials file
+	credentials, err := ioutil.ReadFile(credentialsFile)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read credentials file: %v", err)
+	}
+
+	// Create a Config object from the credentials file
+	config, err := google.ConfigFromJSON(credentials, sheets.SpreadsheetsScope)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse credentials: %v", err)
+	}
+
+	token := &oauth2.Token{
+		// If you have an access token saved in the token.json file, you can assign it here
+		AccessToken: "ya29.a0AWY7CklmInTipDPAw3f8S1wzhBHjd0CkTzNH5rVp7Fc6d6CEWMdGbdrsJjRoB29c1lTt_BmpGF8UyiHSPAlGWzAlo-Ndfc_6utC4BRjGsorW8HYznAD_b16jPNetKtS3ijyR39H_xanEeUFqjjqq8ZvIL426aCgYKAd8SARISFQG1tDrpPppg6SjW_qI8xH9-5M7HiQ0163",
+	}
+
+	// Obtain a token source from the Config
+	tokenSource := config.TokenSource(ctx, token)
+
+	// Create a new HTTP client using the token source
+	client := oauth2.NewClient(ctx, tokenSource)
+
+	// Create a new Google Sheets service client
+	service, err := sheets.New(client)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create Sheets client: %v", err)
+	}
+
+	return service, nil
+}
+
+func getClient(ctx context.Context) (*http.Client, error) {
+	fmt.Println("getclient")
+	tokenFile := "token.json"
+	token, err := ioutil.ReadFile(tokenFile)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read token file: %v", err)
+	}
+	fmt.Println("getclient 1")
+	config, err := google.ConfigFromJSON(token, sheets.SpreadsheetsScope)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse token: %v", err)
+	}
+	fmt.Println("getclient 2")
+	client := config.Client(ctx, &oauth2.Token{
+		AccessToken: string(token),
+	})
+	fmt.Println("getclient 3")
+	return client, nil
 }
 
 func getJob(c *gin.Context) {
@@ -62,8 +205,79 @@ func getJob(c *gin.Context) {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to scan row"})
 			return
 		}
+		job.JobDate = strings.TrimSpace(job.JobDate[:10]) // Slice to get the first 10 characters (date portion)
 		jobs = append(jobs, job)
 	}
 	fmt.Println("end")
+	c.Header("Content-Type", "application/json")
 	c.JSON(http.StatusOK, jobs)
+	// c.JSON(http.StatusOK, jobs)
 }
+
+// func writeData(c *gin.Context) {
+// 	apiKey := "AIzaSyCJNfVJMqWTCLAaWWcElM4t_5cxwj1B8_g"
+
+// 	service, err := createService(apiKey)
+// 	if err != nil {
+// 		log.Fatalf("Unable to create Google Sheets service: %v", err)
+// 		c.JSON(http.StatusInternalServerError, gin.H{
+// 			"error": "Internal Server Error",
+// 		})
+// 		return
+// 	}
+
+// 	spreadsheetID := "YOUR_SPREADSHEET_ID"
+// 	sheetName := "YOUR_SHEET_NAME"
+
+// 	// Parse the request body to get the data
+// 	var data struct {
+// 		JobDate				string `json:"jobDate"`
+// 		ReferenceNo			string `json:"referenceNo"`
+// 		CustomerName		string `json:"customerName"`
+// 		DeliveryLocName		string `json:"deliveryLocName"`
+// 		Remarks             string `json:"remarks"`
+// 		SelectedHappened    string `json:"selectedHappened"`
+// 		SelectedIssue       string `json:"selectedIssue"`
+// 		SelectedSettlement  string `json:"selectedSettlement"`
+// 		SettlementDate  	string `json:"settlementDate"`
+// 		TruckNo  			string `json:"truckNo"`
+// 	}
+// 	if err := c.ShouldBindJSON(&data); err != nil {
+// 		c.JSON(http.StatusBadRequest, gin.H{
+// 			"error": "Invalid request body",
+// 		})
+// 		return
+// 	}
+
+// 	values := [][]interface{}{
+// 		{"jobDate", "referenceNo", "customerName", "deliveryLocName", "Remarks", "selectedHappened", "selectedIssue", "selectedSettlement", "settlementDate", "truckNo"},
+// 		{data.JobDate, data.ReferenceNo, data.CustomerName, data.DeliveryLocName, data.Remarks, data.SelectedHappened, data.SelectedIssue, data.SelectedSettlement, data.SettlementDate, data.TruckNo},
+// 	}
+
+// 	rangeValue := sheetName + "!A1:D2"
+// 	valueRange := &sheets.ValueRange{
+// 		Values: values,
+// 	}
+
+// 	_, err = service.Spreadsheets.Values.Update(spreadsheetID, rangeValue, valueRange).ValueInputOption("USER_ENTERED").Do()
+// 	if err != nil {
+// 		if gErr, ok := err.(*googleapi.Error); ok {
+// 			log.Fatalf("Unable to write data to Google Sheets: %v", gErr.Message)
+// 			c.JSON(http.StatusInternalServerError, gin.H{
+// 				"error": "Internal Server Error",
+// 			})
+// 		} else {
+// 			log.Fatalf("Unable to write data to Google Sheets: %v", err)
+// 			c.JSON(http.StatusInternalServerError, gin.H{
+// 				"error": "Internal Server Error",
+// 			})
+// 		}
+// 		return
+// 	}
+
+// 	c.JSON(http.StatusOK, gin.H{
+// 		"message": "Data written successfully to Google Sheets",
+// 	})
+// }
+
+
